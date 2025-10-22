8d517ef315b072a812e574d1bbeaa28ebf95cc51
Marco Cawthorne <marco@icculus.org>
2025-10-22 10:48:34 -0700

    Implement number-based weapon selection input

--- src/hud/WeaponSelect.h
+++ src/hud/WeaponSelect.h
@@ -24,6 +24,7 @@ public:
 	virtual void SelectSlot(int, bool);
 	virtual void SelectNext(bool);
 	virtual void SelectPrevious(bool);
+	virtual void SelectLast(bool);
 	virtual void DrawBar(vector, float);
 
 	nonvirtual void Event_Opened(void);
